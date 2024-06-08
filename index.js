d0088ee7b2068ae68559a4037538ea570561a22a
sjesus <s.jesus10@yahoo.com>
2024-06-07 21:07:28 -0500

    created pages and routes to the pages

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/list", (req, res) => {
+    res.render("list.ejs");
+});
+
+app.get("/add", (req, res) => {
+    res.render("add.ejs");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
