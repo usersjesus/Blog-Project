bdd7ef32f09fae0725a127d09b04a22381814b81
sjesus <s.jesus10@yahoo.com>
2024-06-09 19:05:55 -0500

    added form for user create a post

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
@@ -18,6 +20,11 @@ app.get("/about", (req, res) => {
 
 app.get("/list", (req, res) => {
     res.render("list.ejs");
+});
+
+app.post("/list", (req, res) => {
+    console.log(req.body);
+    res.redirect("/list");
 });
 
 app.get("/add", (req, res) => {
